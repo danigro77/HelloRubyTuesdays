2f587edd1500a0e0478966219f86f66290e8e7ca
Daniela Grossmann <daniela.grossmann@gmail.com>
2012-07-28 17:06:38 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HelloRubyTuesday::Application.config.secret_token = '88891262fcb64df212940b7fd2566231e7704a254b9926a55a11b7085dcbef0938f828732048eaea3d641824f0f125e23b70618e6a0c4b7f187c3d93badde6e3'
